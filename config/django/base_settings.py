e076cb98e8b9ccecdb045e6a24854cb299cb825e
beak jong seoung <jjong015189@gmail.com>
2024-05-27 11:39:29 +0900

    feat: camelCase to snake_case

--- config/django/base_settings.py
+++ config/django/base_settings.py
@@ -122,6 +122,15 @@ REST_FRAMEWORK = {
     # "DEFAULT_RENDERER_CLASSES": [
     #     "core.renderers.CustomRenderer",
     # ],
+    "DEFAULT_RENDERER_CLASSES": (
+        "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
+        "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        "djangorestframework_camel_case.parser.CamelCaseFormParser",
+        "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
+        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
+    ),
     "DEFAULT_PAGINATION_CLASS": "core.paginations.CustomPagination",
 }
 
